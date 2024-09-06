595d4561acebbc2a46c49d833c7e68dbf3582e13
206620 <thomspon.clarkb@icloud.com>
2024-09-06 11:09:10 -0400

    Working on caesar encode test

--- Part 1 - Formative/1.1 Caesar Shift/tests/test_caesar_encode.py
+++ Part 1 - Formative/1.1 Caesar Shift/tests/test_caesar_encode.py
@@ -4,14 +4,15 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from main import caesar_encode
-class MyTestCase(unittest.TestCase):
+
+
+class MyTestCase(TestCase):
     def test_caesar_encode_uppercase(self):
-        self.assertEqual(caesar_encode(text, n), "MJQQTBTWQI")
+        self.assertEqual(caesar_encode("HELLOWORLD", 5), "MJQQTBTWQI")
 
-    def test_caesar_encode_uppercase(self):
-        self.assertEqual(caesar_encode(text, n), "mjqqtbtwqi")
+    def test_caesar_encode_lowercase(self):
+        self.assertEqual(caesar_encode("HELLOWORLD", 5), "mjqqtbtwqi")
 
+    def test_caesar_encode_lowercase(self):
+        self.assertEqual(caesar_encode("HELLOWORLD", 5), "mjqqtbtwqi")
 
-
-if __name__ == '__main__':
-    unittest.main()

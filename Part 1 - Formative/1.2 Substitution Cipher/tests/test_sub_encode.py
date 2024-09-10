3aa73b4e81a7b1b290bb385f27f5a9ea3e7b89c9
206620 <thomspon.clarkb@icloud.com>
2024-09-10 11:10:21 -0400

    Working on substitution cipher tests

--- Part 1 - Formative/1.2 Substitution Cipher/tests/test_sub_encode.py
+++ Part 1 - Formative/1.2 Substitution Cipher/tests/test_sub_encode.py
@@ -1,10 +1,11 @@
-import unittest
+from unittest import TestCase
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-
-class MyTestCase(unittest.TestCase):
+from main import sub_encode
+class MyTestCase(TestCase):
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
 
 
-if __name__ == '__main__':
-    unittest.main()
